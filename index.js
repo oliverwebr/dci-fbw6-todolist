fd0ad16178064fe81657a877a9c14a641134b5c7
Tommy Spielhoelle <spiel.hoelle@live.de>
2018-02-13 13:11:06 +0100

    Remove item

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ class Todo {
     }
     this.formEventListener()
     this.render()
+    document.addEventListener('click', (e)=>{
+      if(e.target && e.target.classList.contains( 'btn-outline-danger' )){
+        let itemKey = this.findItemKey(e.target.dataset.name)
+        this.updateCart(e.target.dataset.name, true)
+      }
+    })
   }
   formEventListener() {
     this.elements.button.addEventListener('click', (e)=>{
       e.preventDefault(); 
       this.updateCart(this.elements.input.value)
+      this.elements.input.value = ""
     })
   }
   findItemKey(itemName){
@@ -27,12 +34,15 @@ class Todo {
     let itemKey = this.findItemKey(item)
     console.log(this.db)
     console.log(itemKey)
-    if(itemKey === undefined) {
-      this.db.push({title: this.elements.input.value, state: false})
-    } else {
-      console.log("item already there")
+    if(remove) {
+      this.db.splice(itemKey, 1)
+    } else{  
+      if(itemKey === undefined) {
+        this.db.push({title: this.elements.input.value, state: false})
+      } else {
+        console.log("item already there")
+      }
     }
-
     localStorage.setItem("todos", JSON.stringify( this.db ))
     this.render()
   }
@@ -44,6 +54,7 @@ class Todo {
       element.removeAttribute("id");
       element.classList.add("d-flex")
       element.classList.remove("d-none")
+      element.querySelector('.btn-outline-danger').dataset.name = this.db[item].title
 
       element.querySelector('.title').innerHTML = this.db[item].title
       container.appendChild(element);
