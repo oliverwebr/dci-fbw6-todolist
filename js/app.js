7e67c74693ca6e94b767ca3387166865e22079b3
Patrick Schuelke <p-schuelke@gmx.de>
2018-02-13 10:40:07 +0100

    add ability to add and display items

--- js/app.js
+++ js/app.js
@@ -1,30 +1,66 @@
 class ToDo {
   constructor() {
-    this.items = [];
+    // list of todo items
+    this.items = ['Test1', 'TEst2', 'Test3'];
+    this.init();
   }
 
+  init() {
+    this.updateList();
+  }
+
+  updateList() {
+    ui.displayListItems(this.items);
+  }
+
+  // adds ToDo item to array
   addItem(itemToAdd) {
     this.items.push(itemToAdd);
+    this.updateList();
+  }
+}
+
+class UI {
+  constructor() {
+    this.elements = {
+      listContainer: document.querySelector('.itemList'),
+    };
+  }
+
+  displayListItems(list) {
+    this.elements.listContainer.innerHTML = '';
+    list.forEach((item) => {
+      this.elements.listContainer.innerHTML += `
+      <li class="list-group-item">
+        <span class="todo_item">${item}</span>
+        <i class="fa fa-trash todo_delete" aria-hidden="true"></i>
+      </li>
+      `;
+    });
   }
 }
 
 class Controller {
   constructor() {
-    this.init();
+    Controller.init();
   }
 
   // initialize all event listeners
-  init() {
-    this.addToDoListener();
+  static init() {
+    Controller.addToDoListener();
   }
 
   // listens to clicks on Add ToDo Button
-  addToDoListener() {
+  static addToDoListener() {
     const addBtn = document.querySelector('#addToDoBtn');
     const input = document.querySelector('#toDoInput');
-    addBtn.addEventListener('click', () => toDo.addItem(input.value));
+    addBtn.addEventListener('click', () => {
+      toDo.addItem(input.value);
+      input.value = '';
+    });
   }
 }
 
+const ui = new UI();
 const toDo = new ToDo();
 const control = new Controller();
