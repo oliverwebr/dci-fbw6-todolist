9d7675ddf799fc52d9b139f9d9390f5cc215e6bc
Tommy Spielhoelle <spiel.hoelle@live.de>
2018-02-27 20:38:43 +0100

    modularize bootstrap + sass into bundle.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.sass';
+
 class Todo {
   constructor () {
     
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    main: ['./src/index.js']		
+  },
   output: {
-    filename: './bundle.js'
-  }
-};
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          { 
+            loader: 'babel-loader'
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          },						
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { 
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+      
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin()
+  ]
+}
