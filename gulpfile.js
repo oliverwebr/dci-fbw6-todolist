103f6288ed5524c508332f64210df2cf4db32419
Elbehary <ahmedelbehary0@gmail.com>
2018-03-02 15:49:54 +0100

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const sass = require('gulp-sass')
 gulp.task('sass', function () {
   return gulp.src('./src/*.sass')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-
     .pipe(gulp.dest('./dist'));
 });
 
